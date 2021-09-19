9833c981c4677e77192982be1baee9cd81d6662b
Nicolas Rubiano <v-nicolasr@microsoft.com>
2021-09-19 17:32:21 -0500

    add movie entity and add simple db context

---
+++ Rentt.Infrastructure/Persistence/Configurations/RenttDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rentt.Domain.Entities;
+
+namespace Rentt.Infrastructure.Persistence.Configurations
+{
+    public class RenttDbContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
