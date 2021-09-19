ebf7114928c044d892bdeae86d04b50a17d6ac95
Nicolas Rubiano <v-nicolasr@microsoft.com>
2021-09-19 15:20:20 -0500

    clean up the api project

---
+++ src/Rentt.API/Controllers/MoviesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Rentt.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        [HttpGet()]        
+        public async Task<ActionResult> Get() {
+            return Ok();
+        }
+    }
+}
